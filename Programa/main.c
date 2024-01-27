ffd11fca8f0dd4f73f8064e4daadea03dac3079d
tari9925 <tadas.riksas@mif.stud.vu.lt>
2024-01-27 18:34:42 +0200

    wrote all debug printf lines

--- Programa/main.c
+++ Programa/main.c
@@ -5,17 +5,35 @@
 
 
 //TODO tmp fixas kad kompileris nerektu
-void perduoti(FILE*, LIST**){
-
-
-
+void perduoti(FILE* output_file, LIST* data_list){
+    while(data_list != NULL){
+        /* DO YOUR THING HERE */
+        printf("\nBOLD %d:", data_list->data.BOLD);
+        printf("\nITALIC %d:", data_list->data.ITALIC);
+        printf("\nUNDERLINE %d:", data_list->data.UNDERLINE);
+        printf("\nTITLE %d:", data_list->data.TITLE);
+        printf("\nBULLETPOINT %d:", data_list->data.BULLETPOINT);
+        printf("\nUNUSED %d:", data_list->data.UNUSED);
+        printf("\nSEGMENT_NUMBER %d:", data_list->data.SEGMENT_NUMBER);
+        printf("\nTOTAL_SEGMENTS %d:", data_list->data.TOTAL_SEGMENTS);
+        printf("\nTEXTSIZE %d:", data_list->data.TEXTSIZE);
+        printf("\nFONT %s:", data_list->data.FONT);
+        printf("\nID %d:", data_list->data.ID);
+        printf("\nStr %s:", data_list->data.Str);
+        /* END DOING YOUR THINGs*/
+        data_list = data_list->next;
+    }
 }
 //
 
 int main(int argc, char** argv){
-	LIST* l = init();
+    printf("\nargc %d: ", argc);
+    printf("\nargv[0]: %s", argv[0]);
+    printf("\nargv[1]: %s", argv[1]);
+    printf("\nargv[2]: %s", argv[2]);
+    LIST* l = init();
 	if(argc == 1 || argc > 3){
-		printf("Klaidinga įvestis, įveskite duomenų failo pavadinima ir, jei norite pageidaujamą išvesties failo pavadinimą\n");
+		printf("\nKlaidinga įvestis, įveskite duomenų failo pavadinima ir, jei norite pageidaujamą išvesties failo pavadinimą\n");
 	    return 1;
     }
 	FILE *input = fopen(argv[1], "r");
@@ -26,7 +44,7 @@ int main(int argc, char** argv){
 		output = fopen("Output.html", "w");
 	}
 	if(input == NULL){//output nebus NULL tai nzn gal veliau patikrinsiu ar pasibaigia .html or smthn xdd
-		printf("Nu blin...");
+		printf("\nFAILED TO OPEN INPUT FILE!");
         return 1;
 	}
 	long unsigned len = 0;
@@ -35,6 +53,6 @@ int main(int argc, char** argv){
 	while((read = getline(&line, &len, input)) != -1){
 		parseString(line, &l);
 	}
-	perduoti(output, &l);
+	perduoti(output, l);
 	return 0;
 }
